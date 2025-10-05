3f3c91ed42becca25e5a72e5f1e9aba1a5acf919
okauefonseca <124893260+Fonseca-Dev@users.noreply.github.com>
2025-10-05 15:45:38 -0300

    Update App.tsx

--- agi_invest-react-app/src/App.tsx
+++ agi_invest-react-app/src/App.tsx
@@ -2,24 +2,32 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './App/Home/Home';
 import Login from './App/Login/Login';
 import Signup from './App/Signup/Signup';
-import Extract from './App/Extrato/Extract'; // ✅ importar Extract
+import Extract from './App/Extrato/Extract';
+import { SaldoProvider } from './contexts/SaldoContext';
+import { TransacaoProvider } from './contexts/TransacaoContext';
+import { TipoTransacaoProvider } from './contexts/TipoTransacaoContext';
 import './App.css';
 
 function App() {
   return (
     <div className="app-container">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/extrato" element={<Extract />} /> {/* ✅ nova rota */}
-        </Routes>
-      </Router>
+      <SaldoProvider>
+        <TransacaoProvider>
+          <TipoTransacaoProvider>
+            <Router>
+              <Routes>
+                <Route path="/" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/extrato" element={<Extract />} />
+              </Routes>
+            </Router>
+          </TipoTransacaoProvider>
+        </TransacaoProvider>
+      </SaldoProvider>
     </div>
   );
 }
 
 export default App;
-
