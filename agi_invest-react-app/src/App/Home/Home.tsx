80632a02b3b8c06b22d86c36ba07de7e64f38c32
okauefonseca <124893260+Fonseca-Dev@users.noreply.github.com>
2025-10-05 17:07:38 -0300

    Update Home.tsx

--- agi_invest-react-app/src/App/Home/Home.tsx
+++ agi_invest-react-app/src/App/Home/Home.tsx
@@ -16,14 +16,13 @@ const Home: React.FC = () => {
   const [showBalance, setShowBalance] = React.useState(false);
   const {saldo, atualizarSaldo} = useSaldo(); // <-- pegar saldo do contexto
 
-  const [userAvatar, setUserAvatar] = React.useState<string | null>(() => {
-    return localStorage.getItem("userAvatar") || null;
-  });
-
-  const [userName, setUserName] = React.useState<string | null>(() => {
-    return localStorage.getItem("userName") || null;
-  });
+  const [userAvatar] = React.useState<string | null>(() => { 
+    return localStorage.getItem("userAvatar") || null; 
+  }); 
   
+  const [userName] = React.useState<string>(() => { 
+    return localStorage.getItem("userName") || "Usuário"; 
+  });
 
   // Se quiser atualizar saldo ao montar a tela
   React.useEffect(() => {
@@ -553,6 +552,7 @@ const Home: React.FC = () => {
 };
 
 export default Home;
+
 
 
 
