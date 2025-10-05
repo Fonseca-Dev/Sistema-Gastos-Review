3ad95592a9aaa229f1ce09691cf468b1c596d6fa
okauefonseca <124893260+Fonseca-Dev@users.noreply.github.com>
2025-10-05 14:50:06 -0300

    Update Extract.tsx

--- agi_invest-react-app/src/App/Extrato/Extract.tsx
+++ agi_invest-react-app/src/App/Extrato/Extract.tsx
@@ -7,7 +7,7 @@ import { useTipoTransacao } from "../../contexts/TipoTransacaoContext";
 
 const Extract: React.FC = () => {
   const navigate = useNavigate();
-  const {saldo, setSaldo} = useSaldo();
+  const { saldo } = useSaldo();
   const {transacoes} = useTransacao();
   const {renderIcon} = useTipoTransacao();
 
