232cf2c1f0c5b9c7d012af2ca2fbcd64743c6ff0
okauefonseca <124893260+Fonseca-Dev@users.noreply.github.com>
2025-10-05 13:32:54 -0300

    Update Home.tsx

--- agi_invest-react-app/src/App/Home/Home.tsx
+++ agi_invest-react-app/src/App/Home/Home.tsx
@@ -14,7 +14,7 @@ const Home: React.FC = () => {
   const navigate = useNavigate();
   const [showBalance, setShowBalance] = React.useState(false);
   const [saldo, setSaldo] = React.useState<number | null>(null);
-  const [ultimaConta, setUltimaConta] = React.useState<any | null>(null);
+  // const [ultimaConta, setUltimaConta] = React.useState<any | null>(null);
 
   const [userAvatar, setUserAvatar] = React.useState<string | null>(() => {
     return localStorage.getItem("userAvatar") || null;
@@ -36,7 +36,7 @@ const Home: React.FC = () => {
         .then((data) => {
           if (data && data.objeto && data.objeto.length > 0) {
             const conta = data.objeto[data.objeto.length - 1]; // última conta
-            setUltimaConta(conta);
+            //setUltimaConta(conta);
             setSaldo(conta.saldo);
           }
         })
@@ -587,3 +587,4 @@ const Home: React.FC = () => {
 };
 
 export default Home;
+
